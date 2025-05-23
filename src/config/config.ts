fd9cbe96e826fe48479aec6e0415dd2b6943c6af
kwr1337 <rostft@gmail.com>
2025-05-23 13:27:32 +0300

    1

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,3 @@
-export const API_BASE_URL = 'https://kwr1337-time-for-namaz-b-ffe6.twc1.net';
+export const API_BASE_URL = 'https://kwr1337-time-for-namaz-b-de57.twc1.net';
 // export const API_BASE_URL = '';  
 // export const API_BASE_URL = 'http://localhost:8080';
