ce96bded0e0423eeba3e5177248a9b00c581e84b
kwr1337 <xameleon514@gmail.com>
2025-11-21 17:35:49 +0300

    1

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,3 @@
-export const API_BASE_URL = '';
-// export const API_BASE_URL = 'https://45.10.41.224:8080';
-// export const API_BASE_URL = 'http://localhost:8080';1
+export const API_BASE_URL = '/api';
+// export const API_BASE_URL = 'https://45.10.41.224:8080/api';
+// export const API_BASE_URL = 'http://localhost:8080';
