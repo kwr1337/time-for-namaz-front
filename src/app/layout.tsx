93089b599b6ea5739b43abc8dbaa0fa72a68e326
kwr1337 <xameleon514@gmail.com>
2025-12-15 01:20:10 +0300

    1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Noto_Sans } from 'next/font/google'
+import Script from 'next/script'
 import { Toaster } from 'react-hot-toast'
 
 import { SITE_NAME } from '@/constants/seo.constants'
@@ -36,6 +37,33 @@ export default function RootLayout({
 
 					<Toaster position="top-right" />
 				</Providers>
+
+				{/* Yandex.Metrika counter */}
+				<Script
+					id="yandex-metrika"
+					strategy="afterInteractive"
+					dangerouslySetInnerHTML={{
+						__html: `
+							(function(m,e,t,r,i,k,a){
+								m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+								m[i].l=1*new Date();
+								for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+								k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+							})(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=105693238', 'ym');
+							ym(105693238, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+						`
+					}}
+				/>
+				<noscript>
+					<div>
+						<img
+							src="https://mc.yandex.ru/watch/105693238"
+							style={{ position: 'absolute', left: '-9999px' }}
+							alt=""
+						/>
+					</div>
+				</noscript>
+				{/* /Yandex.Metrika counter */}
 			</body>
 		</html>
 	)
